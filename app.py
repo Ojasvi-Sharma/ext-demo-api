c286645996e9b23d13ad951a5e60a0cd15a019ca
Abhishek Nigam <abhisheknigam1996@gmail.com>
2018-04-22 03:38:52 +0530

    Added some endpoints

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 from utils import (
     get_date, 
     get_location, 
     get_energy_values,
     get_energy_values_week, 
-    get_energy_values_month
-    get_room_info
+    get_energy_values_month,
+    get_compare_day,
+    get_compare_week,
+    get_compare_month,
+    get_day_cost,
+    get_day_savings,
+    get_week_cost,
+    get_week_savings,
+    get_month_cost,
+    get_month_savings,
+    get_room_info,
+    get_room_actuator_info,
+    change_ac,
+    change_light
 )
 
 app = Flask(__name__)
@@ -49,15 +61,109 @@ def home_room_info(room_no_str):
 
 @app.route('/history/day', methods=['GET'])
 def history_day():
-    _response = get_energy_values()
+    _response = {
+        "values": get_energy_values(),
+        "cost": get_day_cost(),
+        "savings": get_day_savings()
+    }
     return jsonify(_response)
 
 @app.route('/history/week', methods=['GET'])
 def history_week():
-    _response = get_energy_values_week()
+    _response = {
+        "values": get_energy_values_week(),
+        "cost": get_week_cost(),
+        "savings": get_week_savings()
+    }
     return jsonify(_response)
 
 @app.route('/history/month', methods=['GET'])
 def history_month():
-    _response = get_energy_values_month()
+    _response = {
+        "values": get_energy_values_month(),
+        "cost": get_month_cost(),
+        "savings": get_month_savings()
+    }
     return jsonify(_response)
+
+
+## Compare routes
+
+@app.route('/history/compare/day', methods=['GET'])
+def history_compare_day():
+    _response = get_compare_day()
+    return jsonify(_response)
+
+@app.route('/history/compare/week', methods=['GET'])
+def history_compare_week():
+    _response = get_compare_week()
+    return jsonify(_response)
+
+@app.route('/history/compare/month', methods=['GET'])
+def history_compare_month():
+    _response = get_compare_month()
+    return jsonify(_response)
+
+
+###################
+# Admin endpoints
+###################
+
+@app.route('/admin/initialise', methods=['GET'])
+def admin_initialise():
+    _response = {
+        "date": get_date(),
+        "location": get_location()
+    }
+    return jsonify(_response)
+
+@app.route('/admin/room/<string:room_no>', methods=['GET'])
+def admin_room_info(room_no):
+    _response = get_room_actuator_info(room_no)
+    return jsonify(_response)
+
+@app.route('/admin/room/change/ac/', methods=['POST'])
+def admin_change_ac():
+    request_data = request.get_json()
+    
+    room_no = request_data['room_no']
+    ac_id = request_data['ac_id']
+    increase = request_data['increase']
+   
+    new_temp = change_ac(room_no, ac_id, increase)
+
+    if new_temp is not None:
+        _response = {
+            "status": True,
+            "temp":new_temp
+            }
+    else:
+        _response = {
+            "status": False
+        }
+
+    return jsonify(_response)
+
+
+@app.route('/admin/room/change/light/', methods=['POST'])
+def admin_change_light():
+    request_data = request.get_json()
+    
+    room_no = request_data['room_no']
+    light_id = request_data['light_id']
+   
+    new_state = change_light(room_no, light_id)
+
+    if new_state is not None:
+        _response = {
+            "status": True,
+            "state":new_state
+            }
+    else:
+        _response = {
+            "status": False
+        }
+
+    return jsonify(_response)
+
+app.run(port=3000, debug=True)
--- utils.py
+++ utils.py
@@ -6,16 +6,277 @@ ref_energy_values = [18,19,20.3,21,20.4,19.8,19.2,19.6,19.9,20.2,
                     22.9,24,24.2,25.1]
 
 ref_energy_values_week = [
-                137.26,144.19,164.98,151.11,158.04,123.39,178.84,158.04,158.04,144.19,
-                123.39,151.11,178.84,164.98,123.39,178.84,185.76,171.91,123.39,164.98,
-                178.84,178.84,130.32,137.26,164.98,164.98,137.26,144.19,151.11,171.91,
-                185.76,137.26,178.84,130.32,151.11,137.26,171.91,171.91,151.11,185.76,
-                85.76,185.76,178.84,144.19,178.84,151.11,144.19,123.39,171.91,171.91,
-                137.26,130.32]
+                    137.26,144.19,164.98,151.11,158.04,123.39,178.84,158.04,158.04,144.19,
+                    123.39,151.11,178.84,164.98,123.39,178.84,185.76,171.91,123.39,164.98,
+                    178.84,178.84,130.32,137.26,164.98,164.98,137.26,144.19,151.11,171.91,
+                    185.76,137.26,178.84,130.32,151.11,137.26,171.91,171.91,151.11,185.76,
+                    85.76,185.76,178.84,144.19,178.84,151.11,144.19,123.39,171.91,171.91,
+                    137.26,130.32]
 
 ref_energy_values_month = [
-                544.26,445.56,618.66,643.46,494.66,445.91,
-                494.66,494.66,519.46,544.26,469.86,544.26]
+                    544.26,445.56,618.66,643.46,494.66,445.91,
+                    494.66,494.66,519.46,544.26,469.86,544.26]
+
+rooms = {
+    "0": {
+        "ac": [
+            {
+                "id": 1,
+                "temp": 22
+            },
+            {
+                "id": 2,
+                "temp": 22
+            },
+            {
+                "id": 3,
+                "temp": 22
+            }
+        ],
+        "light": [
+            {
+                "id": 1,
+                "state": True
+            },
+            {
+                "id": 2,
+                "state": False
+            },
+            {
+                "id": 3,
+                "state": True
+            },
+            {
+                "id": 4,
+                "state": True
+            }
+        ]
+    },
+    "1": {
+        "ac": [
+            {
+                "id": 1,
+                "temp": 22
+            },
+            {
+                "id": 2,
+                "temp": 22
+            }
+        ],
+        "light": [
+            {
+                "id": 1,
+                "state": True
+            },
+            {
+                "id": 2,
+                "state": False
+            },
+            {
+                "id": 3,
+                "state": True
+            }
+        ]
+    },
+    "2": {
+        "ac": [
+            {
+                "id": 1,
+                "temp": 23
+            },
+            {
+                "id": 2,
+                "temp": 23
+            }
+        ],
+        "light": [
+            {
+                "id": 1,
+                "state": True
+            },
+            {
+                "id": 2,
+                "state": False
+            },
+            {
+                "id": 3,
+                "state": False
+            },
+            {
+                "id": 4,
+                "state": True
+            }
+        ]
+    },
+    "3": {
+        "ac": [
+            {
+                "id": 1,
+                "temp": 29
+            },
+            {
+                "id": 2,
+                "temp": 29
+            },
+            {
+                "id": 3,
+                "temp": 29
+            }
+        ],
+        "light": [
+            {
+                "id": 1,
+                "state": True
+            },
+            {
+                "id": 2,
+                "state": False
+            }
+        ]
+    },
+    "4": {
+        "ac": [
+            {
+                "id": 1,
+                "temp": 23
+            }
+        ],
+        "light": [
+            {
+                "id": 1,
+                "state": True
+            },
+            {
+                "id": 2,
+                "state": False
+            },
+            {
+                "id": 3,
+                "state": False
+            },
+            {
+                "id": 4,
+                "state": True
+            },
+            {
+                "id": 5,
+                "state": True
+            }
+        ]
+    },
+    "5": {
+        "ac": [
+            {
+                "id": 1,
+                "temp": 23
+            },
+            {
+                "id": 2,
+                "temp": 23
+            }
+        ],
+        "light": [
+            {
+                "id": 1,
+                "state": True
+            },
+            {
+                "id": 2,
+                "state": False
+            },
+            {
+                "id": 3,
+                "state": False
+            },
+            {
+                "id": 4,
+                "state": True
+            }
+        ]
+    },
+    "6": {
+        "ac": [
+            {
+                "id": 1,
+                "temp": 19
+            },
+            {
+                "id": 2,
+                "temp": 19
+            },
+            {
+                "id": 3,
+                "temp": 19
+            }
+        ],
+        "light": [
+            {
+                "id": 1,
+                "state": True
+            },
+            {
+                "id": 2,
+                "state": False
+            },
+            {
+                "id": 3,
+                "state": False
+            },
+            {
+                "id": 4,
+                "state": False
+            }
+        ]
+    },
+    "7": {
+        "ac": [
+            {
+                "id": 1,
+                "temp": 18
+            }
+        ],
+        "light": [
+            {
+                "id": 1,
+                "state": False
+            },
+            {
+                "id": 2,
+                "state": True
+            }
+        ]
+    },
+    "8": {
+        "ac": [
+            {
+                "id": 1,
+                "temp": 23
+            },
+            {
+                "id": 2,
+                "temp": 23
+            }
+        ],
+        "light": [
+            {
+                "id": 1,
+                "state": True
+            },
+            {
+                "id": 2,
+                "state": True
+            },
+            {
+                "id": 3,
+                "state": True
+            },
+            {
+                "id": 4,
+                "state": True
+            }
+        ]
+    }
+}
 
 
 def get_energy_values():
@@ -29,7 +290,7 @@ def get_energy_values():
 
 
 def get_energy_values_week():
-    weeks = dt.today().strftime("%U")
+    weeks = int(dt.today().strftime("%U"))
 
     energy_values = []
 
@@ -101,3 +362,173 @@ def get_room_info(room_no):
 ###################
 # History functions
 ###################
+
+## Savings and cost helpers
+
+def get_day_cost():
+    current_hour = dt.now().hour
+    total_cost = 0
+
+    for i in range(0,current_hour):
+        total_cost = total_cost + ref_energy_values[i]
+    
+    return total_cost
+
+def get_day_savings():
+    today_cost = get_day_cost()
+    yesterday_cost = get_day_cost() * 1.56
+
+    savings = yesterday_cost - today_cost
+
+    return savings
+
+def get_week_cost():
+    current_week = int(dt.today().strftime("%U"))
+
+    return ref_energy_values_week[current_week]
+
+def get_week_savings():
+    current_week = int(dt.today().strftime("%U"))
+
+    if current_week > 1:
+
+        current_week_cost = ref_energy_values_week[current_week]
+        past_week_cost = ref_energy_values_week[current_week-1]
+
+    else:
+        current_week_cost = ref_energy_values_week[current_week]
+        past_week_cost = current_week_cost
+
+    savings = past_week_cost - current_week_cost
+    savings = savings if savings > 0 else -savings
+
+    return savings
+
+def get_month_cost():
+    current_month = dt.now().month
+
+    return ref_energy_values_month[current_month]
+
+def get_month_savings():
+    current_month = dt.now().month
+
+    if current_month > 1:
+
+        current_month_cost = ref_energy_values_month[current_month]
+        past_month_cost = ref_energy_values_month[current_month - 1]
+    
+    else:
+        current_month_cost = ref_energy_values_month[current_month]
+        past_month_cost = current_month_cost
+
+    savings = past_month_cost - current_month_cost
+    savings = savings if savings > 0 else -savings        
+
+
+## Comparision helpers
+
+
+def get_compare_day():
+    current_hour = dt.now().hour
+    
+    if current_hour == 1:
+        return {
+            "0": ref_energy_values[current_hour-1],
+            "-1": 0,
+            "-2": 0,
+        }
+    
+    elif current_hour == 2:
+        return {
+            "0": ref_energy_values[current_hour-1],
+            "-1": ref_energy_values[current_hour-2],
+            "-2": 0,
+        }
+    
+    else:
+        return {
+            "0": ref_energy_values[current_hour-1],
+            "-1": ref_energy_values[current_hour-2],
+            "-2": ref_energy_values[current_hour-3],
+        }
+
+def get_compare_week():
+    current_week = int(dt.today().strftime("%U"))
+
+    if current_week == 1:
+        return {
+            "0": ref_energy_values_week[current_week-1],
+            "-1": 0,
+            "-2": 0,
+        }
+    
+    elif current_week == 2:
+        return {
+            "0": ref_energy_values_week[current_week-1],
+            "-1": ref_energy_values_week[current_week-2],
+            "-2": 0,
+        }
+    
+    else:
+        return {
+            "0": ref_energy_values_week[current_week-1],
+            "-1": ref_energy_values_week[current_week-2],
+            "-2": ref_energy_values_week[current_week-3],
+        }
+
+def get_compare_month():
+    current_month = dt.now().month
+
+    if current_month == 1:
+        return {
+            "0": ref_energy_values_month[current_month-1],
+            "-1": 0,
+            "-2": 0,
+        }
+    
+    elif current_month == 2:
+        return {
+            "0": ref_energy_values_month[current_month-1],
+            "-1": ref_energy_values_month[current_month-2],
+            "-2": 0,
+        }
+    
+    else:
+        return {
+            "0": ref_energy_values_month[current_month-1],
+            "-1": ref_energy_values_month[current_month-2],
+            "-2": ref_energy_values_month[current_month-3],
+        }
+    
+
+###################
+# Admin functions
+###################
+
+def get_room_actuator_info(room_no):
+    return rooms.get(room_no)
+
+def change_ac(room_no, ac_id, increase):
+    room = rooms.get(str(room_no))
+    acs = room.get("ac")
+    for ac in acs:
+        if(ac['id'] == ac_id):
+            if increase == "True":
+                ac["temp"] = ac["temp"] + 1
+            else:
+                ac["temp"] = ac["temp"] - 1
+
+            return ac["temp"]
+    
+    return None
+
+
+def change_light(room_no, light_id):
+    room = rooms.get(str(room_no))
+    lights = room.get("light",[])
+    for light in lights:
+        if(light['id'] == light_id):
+            light['state'] = not light['state']
+        return light['state']
+
+    return None
